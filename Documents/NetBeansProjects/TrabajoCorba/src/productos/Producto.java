bb5fbf5d43574120507abd81701f10e756db9826
Tamayo <Tamayo@DESKTOP-MJ58J5A>
2019-05-08 20:34:20 -0500

    Nuevo Proyecto

---
+++ Documents/NetBeansProjects/TrabajoCorba/src/productos/Producto.java
@@ -0,0 +1,108 @@
+package productos;
+
+import java.sql.*;
+import conexioncorba.Conexion;
+import javax.swing.JOptionPane;
+import org.omg.CORBA.ORB;
+
+public class Producto extends ProductoApp.productoPOA {
+
+    private ORB orb;
+    Conexion conex = new Conexion();
+
+    @Override
+    public boolean insertarProducto(String nombre_producto) {
+        boolean resultado = false;
+        try {
+            String sentenciaSql = "insert into producto (nombre_producto)"
+                    + "values('" + nombre_producto + "')";
+            conex.conectar();
+            Statement st = conex.conex.createStatement();
+            int valor = st.executeUpdate(sentenciaSql);
+            if (valor > 0) {
+                resultado = true;
+            }
+//Se cierran los recursos
+            st.close();
+            conex.conex.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Ocurrio un error al insertar una nueva persona. "
+                    + e.getMessage());
+        }
+        return resultado;
+
+    }
+
+    
+    public String consultarPersona(String nombre_producto) {
+
+        String lista = "";
+        try {
+            String sentenciSql = "Select * from producto where nombre_producot = " +nombre_producto;
+            conex.conectar();
+            Statement st = conex.conex.createStatement();
+            ResultSet rs = st.executeQuery(sentenciSql);
+            while (rs.next()) {
+                lista += rs.getLong(2) + " - "
+                        + rs.getString(3) + " - "
+                        + rs.getString(4) + " - "
+                        + rs.getString(5) + " - "
+                        + rs.getString(6) + " - "
+                        + rs.getString(7);
+            }
+//Se cierran los recursos.
+            rs.close();
+            conex.conex.close();
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Ocurrio un error en el catch: " + ex.getMessage());
+        }
+
+        return lista;
+
+    }
+
+    @Override
+    public boolean eliminarProducto(int id) {
+        boolean resultado = false;
+        try {
+            String sentenciSql = "delete from persona where identificacion = " + id;
+            conex.conectar();
+            Statement st = conex.conex.createStatement();
+            int valor = st.executeUpdate(sentenciSql);
+            if (valor > 0) {
+                resultado = true;
+            }
+//Se cierran los recursos.
+            st.close();
+            conex.conex.close();
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Ocurrio un error en el catch: " + ex.getMessage());
+        }
+        return resultado;
+    }
+
+    @Override
+    public void shutdown() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public ResultSet cargarTablaProducto() {
+        ResultSet resultado = null;
+        try {
+            String query = "Select nombre_producto from producto";
+            conex.conectar();
+            Statement st = conex.conex.createStatement();
+            resultado = st.executeQuery(query);
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Ocurrio un error: " + e.getMessage());
+        }
+        return resultado;
+    }
+
+    @Override
+    public boolean actualizarProducto(String nombre) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ Documents/NetBeansProjects/TrabajoCorba/src/productos/RegistroProducto.java
@@ -0,0 +1,182 @@
+package productos;
+
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Tamayo
+ *
+ */
+public class RegistroProducto extends javax.swing.JFrame {
+
+    /**
+     * Creates new form Usuario
+     */
+    public RegistroProducto() {
+        modeloPersona = new DefaultTableModel(null, getColumn());
+        initComponents();
+        cargarTabla();
+    }
+
+    private String[] getColumn() {
+        String columnas[] = new String[]{"Id", "Nombre del Producto"};
+        return columnas;
+    }
+
+    private void cargarTabla() {
+        Producto objProducto = new Producto();
+        ResultSet resultado = objProducto.cargarTablaProducto();
+        try {
+            Object dato[] = new Object[2];
+            while (resultado.next()) {
+                for (int i = 0; i < 2; i++) {
+                    dato[i] = resultado.getObject(i + 1);
+                }
+                modeloPersona.addRow(dato);
+            }
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Ocurrio un error: " + ex.getMessage());
+        }
+    }
+
+    private DefaultTableModel modeloPersona;
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        txtNombreProducto = new javax.swing.JTextField();
+        btnGuardar = new javax.swing.JButton();
+        btnConsultar = new javax.swing.JButton();
+        btnEliminar = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        tblProducto = new javax.swing.JTable();
+        lblNombre = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        btnGuardar.setText("Guardar");
+        btnGuardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGuardarActionPerformed(evt);
+            }
+        });
+
+        btnConsultar.setText("Consultar");
+
+        btnEliminar.setText("Eliminar");
+
+        tblProducto.setModel(modeloPersona);
+        jScrollPane2.setViewportView(tblProducto);
+
+        lblNombre.setForeground(new java.awt.Color(255, 0, 0));
+        lblNombre.setText("Nombre :");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(32, 32, 32)
+                .addComponent(lblNombre)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(txtNombreProducto, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(118, 118, 118))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(81, 81, 81)
+                        .addComponent(btnConsultar)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnGuardar)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnEliminar)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(77, 77, 77)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblNombre)
+                    .addComponent(txtNombreProducto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(213, 213, 213)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnGuardar)
+                    .addComponent(btnEliminar)
+                    .addComponent(btnConsultar))
+                .addGap(26, 26, 26)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardarActionPerformed
+        Producto objPersona = new Producto();
+
+        String nombre = txtNombreProducto.getText();
+
+        boolean resultado = objPersona.insertarProducto(nombre);
+
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Se inserto Correctamente");
+            cargarTabla();
+        } else {
+            JOptionPane.showMessageDialog(null, "Ocurrio un error en el Sistema");
+        }
+
+        // *** Limpio los Campos ***
+        txtNombreProducto.setText("");
+
+
+    }//GEN-LAST:event_btnGuardarActionPerformed
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(RegistroProducto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(RegistroProducto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(RegistroProducto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(RegistroProducto.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new RegistroProducto().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnConsultar;
+    private javax.swing.JButton btnEliminar;
+    private javax.swing.JButton btnGuardar;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JLabel lblNombre;
+    private javax.swing.JTable tblProducto;
+    private javax.swing.JTextField txtNombreProducto;
+    // End of variables declaration//GEN-END:variables
+}
