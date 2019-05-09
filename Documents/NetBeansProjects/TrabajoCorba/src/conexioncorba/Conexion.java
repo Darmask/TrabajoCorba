7565062a71e23ba31d493dc17468003dee7f8f7b
Tamayo <Tamayo@DESKTOP-MJ58J5A>
2019-05-09 14:54:31 -0500

    se realizo la conexion a la base de datos remotamente

--- Documents/NetBeansProjects/TrabajoCorba/src/conexioncorba/Conexion.java
+++ Documents/NetBeansProjects/TrabajoCorba/src/conexioncorba/Conexion.java
@@ -12,8 +12,9 @@ public class Conexion {
     
     public Connection conectar(){
         try {
+           
             Class.forName("com.mysql.jdbc.Driver");
-            conex = DriverManager.getConnection("jdbc:mysql://localhost/facturacion" , "root" , "");
+            conex = DriverManager.getConnection("jdbc:mysql://sql7.freesqldatabase.com:3306/sql7291210","sql7291210","Yk8GiM2xwf");
             System.out.println("La conexion se realizo con exito");
         } catch (Exception e) {
             System.out.println("No fue posible la conexion." + e.getMessage());
