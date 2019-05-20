fa21d5a7d134f8078b65d555b4773b77bff39975
Tamayo <Tamayo@DESKTOP-MJ58J5A>
2019-05-20 13:44:38 -0500

    Nueva conexion

--- Documents/NetBeansProjects/TrabajoCorba/src/conexioncorba/Conexion.java
+++ Documents/NetBeansProjects/TrabajoCorba/src/conexioncorba/Conexion.java
@@ -14,7 +14,7 @@ public class Conexion {
         try {
            
             Class.forName("com.mysql.jdbc.Driver");
-            conex = DriverManager.getConnection("jdbc:mysql://sql7.freesqldatabase.com:3306/sql7291210","sql7291210","Yk8GiM2xwf");
+            conex = DriverManager.getConnection("jdbc:mysql://sql7.freesqldatabase.com:3306/sql7292505","sql7292505","JLZHpssPD2");
             System.out.println("La conexion se realizo con exito");
         } catch (Exception e) {
             System.out.println("No fue posible la conexion." + e.getMessage());
